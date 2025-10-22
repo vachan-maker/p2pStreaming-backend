bda03c882fec6f435bdab2fdd5ffaa8e9c0c2512
vachan-maker <vachan@tutanota.com>
2025-10-22 20:43:30 +0530

    Setting up multer for fileUpload

---
+++ config/multerConfig.js
@@ -0,0 +1,20 @@
+import multer from 'multer';
+import path from 'path';
+
+//we are creating the disk storage engine
+//diskStorage is a function that takes an object with two functions as args. One is destination and the other is filename
+const storage = multer.diskStorage({
+    destintation:(req,file,cb) => {
+        cb(null,'uploads/videos'); //callback function to tell Multer where to store the file
+    },
+    filename: (req,file,cb) => {
+        const uniqueName = Date.now() + Math.round(Math.random()*1E9);
+        cb(null,uniqueName + path.extname(file.originalname)) //extname is used to keep the original file extension
+    }
+}
+)
+
+
+const upload = multer({storage})
+
+export {upload}
